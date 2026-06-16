c64ac854f93922baf92a0b1d881ef2315664cb4d
Aarish Hasan <hasanarish5@gmail.com>
2026-06-16 23:39:46 +0530

    added the passport localMongoose schema

--- app.js
+++ app.js
@@ -7,6 +7,9 @@ const ejsMate = require("ejs-mate");
 const ExpressError = require("./utils/ExpressError.js");
 const session = require("express-session");
 const flash = require("connect-flash");
+const passport = require("passport");
+const LocalStrategy = require("passport-local");
+const User = require("./models/user.js");
 
 
 const listings = require("./routes/listing.js");
@@ -54,11 +57,35 @@ app.get("/", (req, res) => {
 app.use(session(sessionOptions));
 app.use(flash());
 
+//we are now working with poassport or kind of authntication
+
+app.use(passport.initialize());
+app.use(passport.session());
+passport.use(new LocalStrategy(User.authenticate()));
+
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
+
+
+
+
 app.use((req, res, next) => {
     res.locals.success = req.flash("success");
     res.locals.error = req.flash("error");;
     
     next();
+});
+
+app.get("/demouser",async (req, res) => {
+   let fakeUser = new User({
+    email: "student@gmail.com",
+    username: "delta-student"
+   });
+
+   let registeredUser =  await User.register(fakeUser, "helloworld");
+   res.send(registeredUser);
+
+
 });
 
 
