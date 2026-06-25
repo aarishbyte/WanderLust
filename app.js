7ce29438e6cefb25bddb91764f12a58f73c61d63
Aarish Hasan <hasanarish5@gmail.com>
2026-06-25 23:32:59 +0530

    Configure MongoDB Atlas and session management

--- app.js
+++ app.js
@@ -3,7 +3,6 @@ if(process.env.NODE_ENV != "production"){
 }
 
 
-
 const express = require("express");
 const app = express();
 const mongoose = require("mongoose");
@@ -12,6 +11,7 @@ const methodOverride = require("method-override");
 const ejsMate = require("ejs-mate");
 const ExpressError = require("./utils/ExpressError.js");
 const session = require("express-session");
+const {MongoStore} = require("connect-mongo");
 const flash = require("connect-flash");
 const passport = require("passport");
 const LocalStrategy = require("passport-local");
@@ -22,8 +22,13 @@ const listingRouter = require("./routes/listing.js");
 const reviewRouter = require("./routes/review.js");
 const userRouter = require("./routes/user.js");
 
+const dns=require("dns");
 
-const MONGO_URL = "mongodb://127.0.0.1:27017/wanderlust";
+dns.setServers(["1.1.1.1","8.8.8.8"]);
+
+
+
+const dbUrl = process.env.ATLASDB_URL;
 
 main().then(() => {
     console.log("connected to DB")
@@ -32,7 +37,9 @@ main().then(() => {
 });
 
 async function main(){
-    await mongoose.connect(MONGO_URL);
+    
+    await mongoose.connect(dbUrl);
+   
 };
 
 app.set("view engine", "ejs");
@@ -43,9 +50,24 @@ app.engine("ejs", ejsMate);
 app.use(express.static(path.join(__dirname, "/public")));
 
 
+//now we will talk about mongostore
+const store = MongoStore.create({
+    mongoUrl: dbUrl,
+    crypto: {
+        secret: process.env.SECRET,
+    },
+    touchAfter: 24 * 3600,
+});
+
+store.on("error", ()=>{
+    console.log("ERROR in MONGO SESSION STORE", err);
+});
+
+
 //now we will using the sessions , cookies, or flash
 const sessionOptions = {
-    secret: "mysupersecretcode",
+    store,
+    secret: process.env.SECRET,
     resave: false,
     saveUninitialized: true,
     cookie: {
@@ -60,6 +82,9 @@ const sessionOptions = {
 // app.get("/", (req, res) => {
 //     res.send("Hi, I am root");
 // });
+
+
+
 
 app.use(session(sessionOptions));
 app.use(flash());
@@ -136,3 +161,4 @@ app.use((err, req, res, next) => {
 app.listen(8080, () => {
     console.log("server is listening to port 8080");
 });
+
