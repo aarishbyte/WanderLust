6529bd161c2ede8ec99406896d16541cc24238eb
Aarish Hasan <hasanarish5@gmail.com>
2026-06-26 01:19:54 +0530

    add a object

--- app.js
+++ app.js
@@ -152,6 +152,7 @@ app.use( (req, res, next) => {
 });
 
 app.use((err, req, res, next) => {
+    console.error(err);
     let {statusCode=500, message = "Something went wrong!"} = err;
     res.status(statusCode).render("error.ejs", { message})
     // res.status(statusCode).send(message);
